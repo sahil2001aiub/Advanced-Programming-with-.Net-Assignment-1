024585647dbc436c868ec4311b8cbf7270038077
ACER <ACER@LAPTOP-HLEE57B2>
2024-01-31 19:24:14 +0600

    Add project files.

---
+++ first/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace first.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your education page.";
+
+            return View();
+        }
+
+        public ActionResult Personal()
+        {
+            ViewBag.Message = "Your personal page.";
+
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Your personal page.";
+
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            ViewBag.Message = "Your personal page.";
+
+            return View();
+        }
+    }
+}
